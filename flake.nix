93b6eace7fafe832fa0faab367d44a75e9aed333
Sebastian Rakel <sebastian@devunit.eu>
2026-06-27 20:23:03 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,35 +2,44 @@
   description = "my nur packages";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = { flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
-      systems = lib.systems.flakeExposed;
-      perSystem = { system, ... }:
-        let
-          overlay-unstable = final: prev: {
-            unstable = import inputs.nixpkgs-unstable {
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = lib.systems.flakeExposed;
+        perSystem =
+          { system, ... }:
+          let
+            overlay-unstable = final: prev: {
+              unstable = import inputs.nixpkgs-unstable {
+                inherit system;
+                config.allowUnfree = true;
+              };
+            };
+            overlay-bundler = final: prev: {
+              bundlerApp = prev.bundlerApp.override { ruby = prev.ruby_4_0; };
+            };
+          in
+          {
+
+            _module.args.pkgs = import inputs.nixpkgs {
               inherit system;
               config.allowUnfree = true;
+              overlays = [
+                overlay-unstable
+                overlay-bundler
+              ];
             };
           };
-          overlay-bundler = final: prev: {
-            bundlerApp = prev.bundlerApp.override { ruby = prev.ruby_4_0; };
-          };
-        in {
-
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-            overlays = [ overlay-unstable overlay-bundler ];
-          };
-        };
-      imports = [ ./pkgs/flake-module.nix ];
-    });
+        imports = [ ./pkgs/flake-module.nix ];
+      }
+    );
 }
