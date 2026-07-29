83385d4aacb736abd51ce30384b28b08bf16f8ac
Sebastian Rakel <sebastian@devunit.eu>
2026-07-29 10:33:03 +0200

    Update bambu studio

--- pkgs/bambu-studio/default.nix
+++ pkgs/bambu-studio/default.nix
@@ -1,18 +1,22 @@
-{ appimageTools, fetchurl, pkgs }:
+{
+  appimageTools,
+  fetchurl,
+  pkgs,
+}:
 let
   pname = "bambu-studio";
   version = "2.2.0";
 
   src = fetchurl {
-    url =
-      "https://github.com/bambulab/BambuStudio/releases/download/v02.04.00.70/Bambu_Studio_ubuntu-24.04_PR-8834.AppImage";
-    hash = "sha256-JrwH3MsE3y5GKx4Do3ZlCSAcRuJzEqFYRPb11/3x3r0=";
+    url = "https://github.com/bambulab/BambuStudio/releases/download/v02.07.01.62/BambuStudio_ubuntu24.04-v02.07.01.62-20260616195227.AppImage";
+    hash = "sha256-+pi2CFMt+7uysJMUg6rEHlf7GcF1osx719Uo1eD7soc=";
   };
-in appimageTools.wrapType2 {
+in
+appimageTools.wrapType2 {
   inherit pname version src;
 
-  extraPkgs = pkgs:
-    with pkgs; [
+  extraPkgs =
+    pkgs: with pkgs; [
       webkitgtk_4_1
       curl
       cacert
